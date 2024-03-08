ffe052c2ec85a17140b03fcd011aea39048101f2
Steven Yi <siyuan.yi@vanderbilt.edu>
2024-03-08 00:08:55 -0600

    Add layer control to allow selecting data for different years

--- load-cog.js
+++ load-cog.js
@@ -1,36 +1,57 @@
-// initalize leaflet map
-var map = L.map("map").setView([0, 0], 5);
+// Initialize leaflet map
+const map = L.map("map").setView([0, 0], 5);
 
-// add OpenStreetMap basemap
-L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+// Add OpenStreetMap basemap
+L.tileLayer("https://{s}.tile.osm.org/{z}/{x}/{y}.png", {
   attribution:
-    '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+    '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-var url_to_geotiff_file =
-  "https://vds-land-trust-data-cog.s3.us-east-2.amazonaws.com/cog/CONUS2021_ClipAOI_reprojected_cog.tif";
+// Array of objects to store the year and url of each .tif file
+const rasters = [
+  {
+    year: 2021,
+    url: "https://vds-land-trust-data-cog.s3.us-east-2.amazonaws.com/cog/CONUS2021_ClipAOI_reprojected_cog.tif",
+  },
+  {
+    year: 2019,
+    url: "https://vds-land-trust-data-cog.s3.us-east-2.amazonaws.com/cog/CONUS2019_ClipAOI_reprojected_cog.tif",
+  },
+  {
+    year: 2001,
+    url: "https://vds-land-trust-data-cog.s3.us-east-2.amazonaws.com/cog/CONUS2001_ClipAOI_reprojected_cog.tif",
+  }
+]
 
-parseGeoraster(url_to_geotiff_file).then((georaster) => {
-  console.log("georaster:", georaster);
-  var layer = new GeoRasterLayer({
-    pixelValuesToColorFn: (values) => {
-      // transforming single value into an rgba color
-      const nir = values[0];
-      const palette = georaster.palette[nir];
+// Create a new Layers Control
+let layerControl = L.control.layers().addTo(map);
 
-      if (nir === 0) return;
-      // console.log("nir:", nir);
-      const r = palette[0];
-      const g = palette[1];
-      const b = palette[2];
-      return `rgba(${r},${g},${b}, 1)`;
-    },
-    attribution: "Planet",
-    georaster: georaster,
-    resolution: 128,
-    opacity: 0.5,
+// Parse each file in the rasters array
+rasters.forEach(raster => {
+  parseGeoraster(raster.url).then((georaster) => {
+    const layer = new GeoRasterLayer({
+      pixelValuesToColorFn: (values) => {
+        // transforming single value into an rgba color
+        const nir = values[0];
+        const palette = georaster.palette[nir];
+
+        if (nir === 0) return;
+        // console.log("nir:", nir);
+        const r = palette[0];
+        const g = palette[1];
+        const b = palette[2];
+        return `rgba(${r},${g},${b}, 1)`;
+      },
+      attribution: "Planet",
+      georaster: georaster,
+      resolution: 128,
+      opacity: 0.5,
+    });
+
+    // Add the layer as an overlay in the layers control
+    layerControl.addOverlay(layer, raster.year);
+
+    map.fitBounds(layer.getBounds());
   });
-  layer.addTo(map);
+})
 
-  map.fitBounds(layer.getBounds());
-});
